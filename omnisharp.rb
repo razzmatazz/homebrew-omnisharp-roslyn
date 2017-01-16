782445a59b37ebc7902e1436e6ccfe5692e5927a
Saulius Menkevičius <saulius.menkevicius@gmail.com>
2017-01-16 23:15:34 +0200

    update to release 1.9-beta22

--- omnisharp.rb
+++ omnisharp.rb
@@ -1,12 +1,14 @@
 class Omnisharp < Formula
   desc "Cross platform .NET development in the editor of your choice"
   homepage "http://www.omnisharp.net/"
-  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.9-alpha13/omnisharp-osx-x64-netcoreapp1.0.tar.gz"
-  version "1.9-alpha13"
-  sha256 "7629d61f7d80581287584f197403f5cd4ff355a212b68430a314954a89ddf2b8"
+  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.9-beta22/omnisharp-osx-x64-netcoreapp1.0.tar.gz"
+  version "1.9-beta22"
+  sha256 "8adcea91b1f905605f3c242e8ad71952b0a627e7187e3a9af234b8b51ea788b4"
 
   depends_on "mono" => :recommended
-  depends_on "aspnet/dnx/dnvm" => :recommended
+# not quite sure dnvm is a hard requirement -- uncommenting for now
+# because it breaks the forula for me at the moment
+#  depends_on "aspnet/dnx/dnvm" => :recommended
 
   def install
     libexec.install Dir["*"]
